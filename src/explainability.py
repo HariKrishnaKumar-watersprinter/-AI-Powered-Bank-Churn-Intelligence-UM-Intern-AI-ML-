5f7fc2387ecd8e0cd7228822a37e1469e5e54401
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-03-30 11:28:52 +0530

    Update explainability.py

--- src/explainability.py
+++ src/explainability.py
@@ -3,12 +3,12 @@ import pandas as pd
 import joblib
 from src.feature_engineering import create_features
 from src.preprocessing import scale_features,preprocess_data
-
+import os
 def get_shap_values():
     df=create_features()
     numeric_pipeline,x_train_scaled,x_test_scaled=scale_features()
     
-    pipeline = joblib.load("F:/Project/unified mentor/Bank churn Prediction/model/GradientBoosting_AllKNN.pkl")
+    pipeline = os.path.join(os.getcwd(), "best model", "GradientBoosting_AllKNN.pkl")
     model = pipeline.named_steps["model"]
     
     

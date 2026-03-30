cec4cb803f8d14735067192ef87500b531e8cda0
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-03-30 11:01:25 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    path=r'F:\Project\unified mentor\Bank churn Prediction\data\European_Bank.csv'
+    path=r'https://raw.github.com/HariKrishnaKumar-watersprinter/-AI-Powered-Bank-Churn-Intelligence-UM-Intern-AI-ML-/main/data/European_Bank.csv'
     
     df = pd.read_csv(path)
     return df

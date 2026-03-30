cdd5376b0e453c661d6671b367ccaf4bdd51fc86
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-03-30 21:38:08 +0530

    Update bivariate.py

--- dashboard/bivariate.py
+++ dashboard/bivariate.py
@@ -19,4 +19,4 @@ def show(df):
         fig = px.bar(col3, x=col1,y="Count",color=col2,barmode="group",title=f"{col1} vs Churn")
         fig.update_traces(texttemplate='%{y:,.0f}', textposition='inside')
 
-    st.plotly_chart(fig, width='stretch')
+    st.plotly_chart(fig, width ='stretch')

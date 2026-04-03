112b5a98ae7b2e77d9c1843b105ed4a367be3061
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-04-03 21:37:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import joblib
-from src.data_loader import load_data
 from src.data_quality import data_quality_report, detect_outliers
 from src.feature_engineering import create_features
 from utils.retention_engine import personalized_strategy
@@ -42,7 +41,7 @@ h1, h2, h3 {
 # 📂 LOAD DATA + MODEL
 @st.cache_data
 def get_processed_data():
-    df = load_data()
+    
     df = create_features()
     return df
 

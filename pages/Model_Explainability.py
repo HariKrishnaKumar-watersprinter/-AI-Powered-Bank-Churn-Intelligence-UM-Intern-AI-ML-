3926243ff6740c9c12eea409c6c56996aac1624a
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-04-03 11:36:57 +0530

    Update Model_Explainability.py

--- pages/Model_Explainability.py
+++ pages/Model_Explainability.py
@@ -4,28 +4,31 @@ from src.explainability import get_shap_values
 from src.feature_engineering import create_features
 import streamlit as st
 import matplotlib.pyplot as plt
-
+from src.preprocessing import scale_features
+from prediction.predict_model import load_prediction_model
 #if not st.session_state.get('authentication_status'):
     #st.switch_page("app.py")
 
 def model_explainability():
     st.title("Model Explainability")
     st.header("🧠 SHAP Explainability")
-    df=create_features()
-    X = df.drop("Exited", axis=1)
-    sample = X.sample(200)
+    model = load_prediction_model()
+    _,x_train_scaled,_= scale_features()
+    sample = x_train_scaled.sample(200)
 
-    shap_values = get_shap_values()
+    # Using the generic Explainer to support multi-class GradientBoosting
+    explainer = shap.Explainer(model.named_steps['model'].predict, sample)
+    shap_values = explainer(sample)
+
 
     st.subheader("Feature Importance")
     fig = shap.plots.bar(shap_values, show=False)
     fig = plt.gcf()
     st.pyplot(fig)
     plt.clf()
-    
-    st.subheader("Summary Plot")
-    fig2 = shap.plots.beeswarm(shap_values, show=False)
-    fig2 = plt.gcf()
-    st.pyplot(fig2)
+    st.subheader("Global Feature Importance")
+
+    fig, ax = plt.subplots()
+    shap.summary_plot(shap_values, sample, show=False, class_names=model.classes_)
+    st.pyplot(fig)
     plt.clf()
-model_explainability()
